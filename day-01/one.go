c2234efc205150e9e16abb800fe4bca454dfd9a1
mmaasriera <miquelmasriera@gmail.com>
2025-12-01 23:48:39 +0100

    day01 one

--- day-01/one.go
+++ day-01/one.go
@@ -2,12 +2,41 @@
 package main
 
 import (
-	"fmt"
 	"mikicode/aoc25/utils"
+	"strconv"
+	"fmt"
 )
 
 func main() {
-	lines := utils.ReadLines("./inputs/test.txt")
+	var zeroSum int = 0
+	var position int = 50
+	lines := utils.ReadLines("./inputs/one.txt")
 
-	fmt.Println(lines)
+	for _, line := range lines {
+		direction := line[0:1]
+		rawValue, _ := strconv.Atoi(line[1:])
+		value := rawValue % 100
+
+		if direction == "L" {
+			position -= value
+
+			if position < 0 {
+				position = 100 + position
+			}
+		} else if direction == "R" {
+			position += value
+
+			if position > 99 {
+				position = position - 100
+			}
+		}
+
+		if position == 0 {
+			zeroSum++
+		}
+	}
+
+	fmt.Println("result:", zeroSum)
+	// test -> 3
+	// one -> 1084
 }
