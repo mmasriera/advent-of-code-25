3fcf8c3db6293f38e5f2525075f49a670a237e08
mmaasriera <miquelmasriera@gmail.com>
2025-12-13 19:32:23 +0100

    wip

--- day-04/one/main.go
+++ day-04/one/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"mikicode/aoc25/utils"
+)
 
+func getAccessableRolls(rows []string) int {
+	count := 0
+
+	for y, row := range rows {
+		for x, item := range row {
+			fmt.Println(y, x, item == '.')
+		}
+	}
+
+	return count
+}
+
+func main() {
+	rows := utils.ReadLines(utils.GetInputPath())
+	accessableRolls := getAccessableRolls(rows)
+
+	fmt.Println("result:", accessableRolls)
 }
