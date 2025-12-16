dbca5ca72eacb4f03f98a99531c294b665f05d36
mmaasriera <miquelmasriera@gmail.com>
2025-12-16 19:22:15 +0100

    day5

---
+++ day-05/one/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"mikicode/aoc25/utils"
+)
+
+func main() {
+	rows := utils.ReadLines(utils.GetInputPath())
+
+	fmt.Println("result:", rows)
+	// test:13 input:1493
+}
