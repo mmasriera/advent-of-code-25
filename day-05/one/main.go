c6b1202fa8a02ffa508a9684a8eccf3fccaaf587
mmaasriera <miquelmasriera@gmail.com>
2025-12-16 19:41:42 +0100

    parsing

--- day-05/one/main.go
+++ day-05/one/main.go
@@ -3,11 +3,33 @@ package main
 import (
 	"fmt"
 	"mikicode/aoc25/utils"
+	"strings"
 )
+
+type IdRange struct {
+	Min string
+	Max string
+}
 
 func main() {
 	rows := utils.ReadLines(utils.GetInputPath())
+	var ranges []IdRange
+
+	rangesMode := true
+	for _, row := range rows {
+		if row == "" {
+			rangesMode = false
+		} else if rangesMode {
+			rangeItems := strings.Split(row, "-")
+
+			ranges = append(ranges, IdRange{rangeItems[0], rangeItems[1]})
+
+			fmt.Println(ranges)
+		} else if rangesMode == false {
+			fmt.Println(row)
+		}
+	}
 
 	fmt.Println("result:", rows)
-	// test:13 input:1493
+	// test:3 input:
 }
