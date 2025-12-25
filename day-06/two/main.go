b86d49ac68d2db9c7700121fb4f98fc5d8bb11b1
mmaasriera <miquelmasriera@gmail.com>
2025-12-25 19:29:16 +0100

    day6 part2

---
+++ day-06/two/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"mikicode/aoc25/utils"
+	"strconv"
+)
+
+// TO DO: traverse matrix?
+
+func isEmptyColumn(matrix [][]rune, col, numRows int) bool {
+	for i := 0; i < numRows; i += 1 {
+		if matrix[i][col] != ' ' {
+			return false
+		}
+	}
+
+	return true
+}
+
+func calculate(numbers []int, operation rune) int {
+	result := 0
+	if operation == '*' {
+		result = 1
+	}
+
+	for _, number := range numbers {
+		switch operation {
+		case '+':
+			result += number
+		case '*':
+			if number != 0 {
+				result *= number
+			}
+		}
+	}
+
+	return result
+}
+
+func main() {
+	matrix := utils.ReadMatrix(utils.GetInputPath())
+
+	rows := len(matrix)
+	cols := len(matrix[0])
+
+	var operation rune
+	var numbers []int
+	result := 0
+
+	for j := range cols {
+		if isEmptyColumn(matrix, j, rows) {
+			result += calculate(numbers, operation)
+			numbers = nil // reset numbers
+		}
+
+		digits := ""
+
+		for i := range rows {
+			item := matrix[i][j]
+
+			switch item {
+			case ' ':
+				continue
+			case '*':
+				operation = item
+			case '+':
+				operation = item
+			default:
+				digits += string(item)
+			}
+		}
+
+		number, _ := strconv.Atoi(digits)
+		numbers = append(numbers, number)
+	}
+
+	// last operation --> TO DO: sth better
+	result += calculate(numbers, operation)
+
+	fmt.Println("result:", result)
+	// test:3263827 input:7996218225744
+}
