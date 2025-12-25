9997766011088d1b547bcc7bdbad7514d667483d
mmaasriera <miquelmasriera@gmail.com>
2025-12-25 01:11:22 +0100

    day5 part 2

--- day-05/two/main.go
+++ day-05/two/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"mikicode/aoc25/utils"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -11,10 +13,35 @@ type IdRange struct {
 	Min, Max int
 }
 
+func countFreshIds(ranges []IdRange) int {
+	count := 0
+
+	slices.SortFunc(ranges, func(a, b IdRange) int {
+		return cmp.Compare(a.Min, b.Min)
+	})
+
+	prevMax := 0
+
+	for _, r := range ranges {
+		if prevMax > r.Max {
+			continue
+		}
+
+		if prevMax >= r.Min { // overlapping -> remove the overlap
+			count += r.Max - prevMax
+		} else {
+			count += r.Max - r.Min + 1 // no overlapping -> count the starting item
+		}
+
+		prevMax = r.Max
+	}
+
+	return count
+}
+
 func main() {
 	rows := utils.ReadLines(utils.GetInputPath())
 	var ranges []IdRange
-	count := 0
 
 	for _, row := range rows {
 		if row == "" {
@@ -23,13 +50,15 @@ func main() {
 
 		elems := strings.Split(row, "-")
 
-		if len(elems) == 2 {
+		if len(elems) == 2 { // only ranges
 			min, _ := strconv.Atoi(elems[0])
 			max, _ := strconv.Atoi(elems[1])
 			ranges = append(ranges, IdRange{min, max})
 		}
 	}
 
-	fmt.Println("result:", count, ranges)
-	// test:14 input:
+	count := countFreshIds(ranges)
+
+	fmt.Println("result:", count)
+	// test:14 input:352946349407338
 }
