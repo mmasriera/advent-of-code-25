2a70985433227dfa05deedfe4db252271fb81845
mmaasriera <miquelmasriera@gmail.com>
2025-12-29 17:45:34 +0100

    day7 part 1

--- day-07/one/main.go
+++ day-07/one/main.go
@@ -6,19 +6,48 @@ import (
 	"strings"
 )
 
-func countSplits(rows []string) int {
-	result := 0
+type Position struct {
+	Row, Col int
+}
 
-	idx := strings.Index(rows[0], "S")
-
-	for i, row := range rows[:1] {
-		for j, col := range row {
+func appendUniquePosition(list []Position, item Position) []Position {
+	for _, pos := range list {
+		if (pos.Row == item.Row) && (pos.Col == item.Col) {
+			return list
 		}
 	}
 
-	fmt.Println(idx)
+	return append(list, item)
+}
 
-	return result
+func countSplits(rows []string) int {
+	count := 0
+	var items []Position // TO DO: use only the column idx --> []Position vs []int
+
+	for i := range len(rows) {
+		if i == 0 {
+			startIdx := strings.Index(rows[0], "S")
+			items = append(items, Position{Row: 1, Col: startIdx})
+
+			continue
+		}
+
+		var newItems []Position
+		for _, item := range items {
+			if string(rows[item.Row][item.Col]) == "^" { // split -> check if split in border?
+				count += 1
+				newItems = appendUniquePosition(newItems, Position{Row: item.Row + 1, Col: item.Col - 1})
+				newItems = appendUniquePosition(newItems, Position{Row: item.Row + 1, Col: item.Col + 1})
+
+			} else {
+				newItems = appendUniquePosition(newItems, Position{Row: item.Row + 1, Col: item.Col})
+			}
+		}
+
+		items = newItems
+	}
+
+	return count
 }
 
 func main() {
@@ -26,5 +55,5 @@ func main() {
 	result := countSplits(rows)
 
 	fmt.Println("result:", result)
-	// test:21 input:
+	// test:21 input:1587
 }
